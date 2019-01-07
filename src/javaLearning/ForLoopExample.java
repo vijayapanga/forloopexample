70a504478bf2fae288f79006c8fd047fd180ecef
Vijaya Panga <VijayalakshmiPanga@gmail.com>
2019-01-07 16:08:41 +0000

    forloop_example

---
+++ src/javaLearning/ForLoopExample.java
@@ -0,0 +1,11 @@
+package javaLearning;
+
+public class ForLoopExample {
+    public static void main(String[] args) {
+        for(int i=50;i>0;i--){
+            System.out.println("Print "+i);
+       //     break;
+
+        }
+    }
+}
